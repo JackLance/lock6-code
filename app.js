bbacfe89f546576854fdf9db42a9f7f1db8d94f3
JackLance <jack.l.lance@gmail.com>
2018-09-26 09:50:27 -0400

    Update app.js

--- app.js
+++ app.js
@@ -60,6 +60,32 @@ function getDBCredentialsUrl(jsonData) {
 }
 
 function initDBConnection() {
+	    //When running on Bluemix, this variable will be set to a json object
+    //containing all the service credentials of all the bound services
+    if (process.env.VCAP_SERVICES) {
+        dbCredentials.url = getDBCredentialsUrl(process.env.VCAP_SERVICES);
+    } else { //When running locally, the VCAP_SERVICES will not be set
+
+        // When running this app locally you can get your Cloudant credentials
+        // from Bluemix (VCAP_SERVICES in "cf env" output or the Environment
+        // Variables section for an app in the Bluemix console dashboard).
+        // Once you have the credentials, paste them into a file called vcap-local.json.
+        // Alternately you could point to a local database here instead of a
+        // Bluemix service.
+        // url will be in this format: https://username:password@xxxxxxxxx-bluemix.cloudant.com
+        dbCredentials.url = getDBCredentialsUrl(fs.readFileSync("vcap-local.json", "utf-8"));
+    }
+
+    cloudant = require('cloudant')(dbCredentials.url);
+
+    // check if DB exists if not create
+    cloudant.db.create(dbCredentials.dbName, function(err, res) {
+        if (err) {
+            console.log('Could not create new db: ' + dbCredentials.dbName + ', it might already exist.');
+        }
+    });
+
+    db = cloudant.use(dbCredentials.dbName);
 }
 
 initDBConnection();
